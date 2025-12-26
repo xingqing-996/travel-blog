6114a4c747f963cb344d871c9c48dcbaff88021f
xingqing-996 <2448460250@qq.com>
2025-12-26 19:37:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,14 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  // 新增：适配Vercel部署的base路径配置（核心解决404的关键）
+
   base: '/', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  // 可选：确保构建输出目录为dist（Vercel默认识别dist，避免路径识别错误）
+
   build: {
     outDir: 'dist'
   }
